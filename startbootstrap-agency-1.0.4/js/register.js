0e8ba11904a1bbbc36aa4807388df18d2ab91b4c
jose mauricio delgado <jdelgado2002@gmail.com>
2016-05-09 23:19:41 -0500

    adding plans

--- startbootstrap-agency-1.0.4/js/register.js
+++ startbootstrap-agency-1.0.4/js/register.js
@@ -21,3 +21,19 @@ $("#registerButton").click(function(){
         //alert("Data: " + data + "\nStatus: " + status);
     });
 });
+
+$("#registerFree").click(function(){
+    ga('send', 'event', 'Buttons', 'click', 'registerFree');
+});
+                         
+$("#registerHosted").click(function(){
+    ga('send', 'event', 'Buttons', 'click', 'registerHosted');
+});
+                           
+$("#registerEnterprise").click(function(){
+    ga('send', 'event', 'Buttons', 'click', 'registerEnterprise');
+});
+                               
+$("#registerOnPremise").click(function(){
+    ga('send', 'event', 'Buttons', 'click', 'registerOnPremise');
+});
